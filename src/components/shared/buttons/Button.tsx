af477beafca396c7c9b1730a7af1176f943f740b
romandelcerro <mario.romandelcerro@scalian.com>
2025-06-11 16:00:05 +0200

    feat: LinkButton component added

--- src/components/shared/buttons/Button.tsx
+++ src/components/shared/buttons/Button.tsx
@@ -1,22 +1,16 @@
+import type { ImageMetadata } from "astro";
 import type { JSX } from "preact";
 
 interface ButtonProps extends JSX.HTMLAttributes<HTMLButtonElement> {
   id?: string;
   text?: string;
   variant?: "primary" | "secondary" | "blue" | "red" | "green";
-  icon?: string;
+  icon?: string | ImageMetadata;
   iconPosition?: "left" | "top" | "right" | "bottom";
   props?: JSX.HTMLAttributes<HTMLButtonElement>;
 }
 
-const Button = ({
-  id,
-  text = "Click me",
-  variant = "primary",
-  icon,
-  iconPosition,
-  ...props
-}: ButtonProps): JSX.Element => {
+const Button = ({ id, text, variant = "primary", icon, iconPosition = "left", ...props }: ButtonProps): JSX.Element => {
   const colors = {
     primary: "var(--color-jacksons-purple-500)",
     secondary: "var(--color-snuff-500)",
@@ -27,28 +21,36 @@ const Button = ({
   const backgroundColor = colors[variant] || colors.primary;
 
   // Manejo de eventos por ID
-  const handleClick = () => {
-    if (id === "boton-uno") {
-      console.log(`On click del boton 1`);
-    }
-  };
+  const handleClick = () => {};
 
   return (
     <button
       class={`btn btn-${variant}`}
       style={{
-        cursor: "pointer",
+        all: "unset",
+        alingItems: "center",
+        justifyContent: "center",
+        gap: "0.5rem",
+        display: "inline-flex",
+        padding: "0rem 2rem",
+        textAlign: "center",
         clipPath: "polygon(0 100%, 10% 0, 100% 0, 100% 0, 90% 100%, 0 100%)",
         borderTop: "3px solid black",
         borderBottom: "3px solid black",
         borderRadius: "5px",
         backgroundColor,
-        color: "white",
+        cursor: "pointer",
       }}
       onClick={handleClick}
       {...props}
     >
-      {text}
+      {icon && iconPosition === "left" && (
+        <img src={typeof icon === "string" ? icon : icon?.src} alt="" style={{ height: "1.5em" }} />
+      )}
+      <span>{text}</span>
+      {icon && iconPosition === "right" && (
+        <img src={typeof icon === "string" ? icon : icon?.src} alt="" style={{ height: "1.5em" }} />
+      )}
     </button>
   );
 };
